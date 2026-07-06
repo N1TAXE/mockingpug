db3f28e8d19cdb8843957741e2249160ad0ff700
N1TAXE <n1t4x3@gmail.com>
2026-07-06 14:45:10 +0200

    Added Vercel Analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 import { siteUrl } from '@/lib/shared';
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -26,6 +27,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Analytics />
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
